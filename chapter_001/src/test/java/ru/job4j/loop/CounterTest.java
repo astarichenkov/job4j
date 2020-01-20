ad707619bd9c55bcb5e49bab90627eb08cd76f78
Anton Starichenkov <anton.starichenkov@gmail.com>
2020-01-20 23:55:28 +0300

    5.2. Создать программу, вычисляющую факториал.[#224304] - испарвил тест.

--- chapter_001/src/test/java/ru/job4j/loop/CounterTest.java
+++ chapter_001/src/test/java/ru/job4j/loop/CounterTest.java
@@ -12,10 +12,12 @@ public class CounterTest {
     @Test
     public void whenSumEvenNumbersFromOneToTenThenThirty() {
         //напишите здесь тест, проверяющий, что сумма чётных чисел от 1 до 10 при вызове метода counter.add будет равна 30.
+        Counter check = new Counter();
         int start = 1;
         int finish = 10;
-        int out = 30;
-        Assert.assertEquals(start, finish, out);
+        int expected = 30;
+        int out = check.add(start,finish);
+        Assert.assertEquals(expected, out);
     }
 }
 
