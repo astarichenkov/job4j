b7189c2dc491cf30a70153293d424209371516c6
Anton Starichenkov <anton.starichenkov@gmail.com>
2020-01-22 00:46:38 +0300

    5.8. JavaDoc. Code convention[#224306]

--- chapter_001/src/main/java/ru/job4j/calculator/Calculator.java
+++ chapter_001/src/main/java/ru/job4j/calculator/Calculator.java
@@ -1,5 +1,13 @@
 package ru.job4j.calculator;
 
+/**
+ * Class Класс для вычисления арифметических операций + - * /
+ *
+ * @author Anton Starichenkov
+ * @version 1
+ * @since 22.01.2020
+ */
+
 public class Calculator {
 
     public static void add(double first, double second) {
@@ -22,10 +30,14 @@ public class Calculator {
         System.out.println(first + " - " + second + " = " + result);
     }
 
+    /**
+     * Main
+     * @param args - args.
+     */
     public static void main(String[] args) {
         add(1, 1);
-        div(4,2);
-        multiply(2,1);
-        subtract(10,5);
+        div(4, 2);
+        multiply(2, 1);
+        subtract(10, 5);
     }
 }
---
+++ chapter_001/src/main/java/ru/job4j/calculator/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Package for calculate task.
+ *
+ * @author Anton Starichenkov (anton.starichenkov@gmail.com)
+ * @version 1
+ * @since 22.01.2020
+ */
+package ru.job4j.calculator;
