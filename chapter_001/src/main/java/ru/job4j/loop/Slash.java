c2a25289bccf88a27aecda06e6645a5bdad8c8ce
Anton Starichenkov <anton.starichenkov@gmail.com>
2020-01-22 10:52:38 +0300

    5.9. Крест в псевдографике. [#224302]

--- chapter_001/src/main/java/ru/job4j/loop/Slash.java
+++ chapter_001/src/main/java/ru/job4j/loop/Slash.java
@@ -4,7 +4,7 @@ public class Slash {
     public static void draw(int size) {
         for (int row = 0; row < size; row++) {
             for (int cell = 0; cell < size; cell++) {
-                boolean left =row == cell; // добавить условие, по которому нужно определить ставить ли символ или нет.
+                boolean left = row == cell; // добавить условие, по которому нужно определить ставить ли символ или нет.
                 boolean right = size - cell - 1 == row; // добавить условие, что нужно ставить элемент в правый угол.
                 if (left) {
                     System.out.print("0");
