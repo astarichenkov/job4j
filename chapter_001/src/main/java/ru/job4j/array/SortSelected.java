bca62ede5cb183803ca50f7980d0130d3caa46cc
Anton Starichenkov <anton.starichenkov@gmail.com>
2020-01-23 10:51:48 +0300

    6.5.3. Сортировка выборкой.[#224247]

---
+++ chapter_001/src/main/java/ru/job4j/array/SortSelected.java
@@ -0,0 +1,19 @@
+package ru.job4j.array;
+
+public class SortSelected {
+
+    public static int[] sort(int[] data) {
+
+        for (int i = 0; i < data.length; i++) {
+            int min = MinDiapason.findMin(data, i, data.length - 1);
+            int minIndex = FindLoop.indexOf(data, min, i, data.length - 1);
+            if (i != minIndex) {
+                int tmp = data[i];
+                data[i] = data[minIndex];
+                data[minIndex] = tmp;
+            }
+
+        }
+        return data;
+    }
+}
