17495572c98a1090872e43cdee7cb001b9adc5ce
Anton Starichenkov <anton.starichenkov@gmail.com>
2020-02-02 00:00:21 +0300

    5. Аннотация @Override[#224426]

--- chapter_002/src/main/java/ru/job4j/inheritance/JSONReport.java
+++ chapter_002/src/main/java/ru/job4j/inheritance/JSONReport.java
@@ -2,6 +2,7 @@ package ru.job4j.inheritance;
 
 public class JSONReport extends HtmlReport {
 
+
     @Override
     public String generate(String name, String body) {
         return "name : " + name
