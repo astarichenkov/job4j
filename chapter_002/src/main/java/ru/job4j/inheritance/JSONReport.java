8baa38a87f39304efb37f0dbdb8b8a8f043b0a52
Anton Starichenkov <anton.starichenkov@gmail.com>
2020-02-01 23:58:13 +0300

    4. Переопределение[#224424]

---
+++ chapter_002/src/main/java/ru/job4j/inheritance/JSONReport.java
@@ -0,0 +1,15 @@
+package ru.job4j.inheritance;
+
+public class JSONReport extends HtmlReport {
+
+    @Override
+    public String generate(String name, String body) {
+        return "name : " + name
+                + "\nbody : " + body;
+    }
+
+
+    public static void main(String[] args) {
+        System.out.println(new JSONReport().generate("Name", "Body"));
+    }
+}
