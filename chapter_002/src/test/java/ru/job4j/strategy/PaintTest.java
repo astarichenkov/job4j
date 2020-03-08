4360ff00c20ea88112446fe73efd2f102607aaea
Anton Starichenkov <anton.starichenkov@gmail.com>
2020-03-09 00:06:08 +0300

    9.1 Рефакторинг теста @Before @After[#224361]

--- chapter_002/src/test/java/ru/job4j/strategy/PaintTest.java
+++ chapter_002/src/test/java/ru/job4j/strategy/PaintTest.java
@@ -1,25 +1,40 @@
 package ru.job4j.strategy;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
+import java.util.StringJoiner;
 
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
-import java.util.StringJoiner;
-
 
 public class PaintTest {
+    // поле содержит дефолтный вывод в консоль.
+    private final PrintStream stdout = System.out;
+    // буфер для результата.
+    private final ByteArrayOutputStream out = new ByteArrayOutputStream();
+
+    @Before
+    public void loadOutput() {
+        System.out.println("execute before method");
+        System.setOut(new PrintStream(this.out));
+    }
+
+    @After
+    public void backOutput() {
+        System.setOut(this.stdout);
+        System.out.println("execute after method");
+    }
+
     @Test
     public void whenDrawSquare() {
-        PrintStream stdout = System.out;
-        ByteArrayOutputStream out = new ByteArrayOutputStream();
-        System.setOut(new PrintStream(out));
-        new Paint().draw(new Square());
+        Square square = new Square();
         assertThat(
-                new String(out.toByteArray()),
+                square.draw(),
                 is(
                         new StringJoiner(System.lineSeparator())
                                 .add("+++++++")
@@ -29,6 +44,24 @@ public class PaintTest {
                                 .toString()
                 )
         );
-        System.setOut(stdout);
+    }
+
+    @Test
+    public void whenDrawTriangle() {
+        Triangle triangle = new Triangle();
+        assertThat(
+                triangle.draw(),
+                is(
+                        new StringJoiner(
+                                System.lineSeparator())
+                                .add("+")
+                                .add("+++")
+                                .add("+++++")
+                                .add("+++++++")
+                                .add("+++++++++")
+                                .add("+++++++++++")
+                                .toString()
+                )
+        );
     }
 }
