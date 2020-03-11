f383a0eabe3a4d0d1694a3ddc321c08011056868
Anton Starichenkov <anton.starichenkov@gmail.com>
2020-03-12 00:51:36 +0300

    0.4. Пользовательские исключения.[#224384]

---
+++ chapter_002/src/main/java/ru/job4j/ex/FindEl.java
@@ -0,0 +1,25 @@
+package ru.job4j.ex;
+
+public class FindEl {
+    private static String[] value = {"Petr", "Ivan", "Nik", "Vasya"};
+    public static int indexOf(String[] value, String key) throws ElementNotFoundException {
+        int rsl = -1;
+        for (int i = 0; i < value.length; i++) {
+            if (value[i].equals(key)) {
+                rsl = i;
+            }
+        }
+        if (rsl == -1) {
+            throw new ElementNotFoundException("Element not found.");
+        }
+        return rsl;
+    }
+
+    public static void main(String[] args) {
+        try {
+            indexOf(value, "Anton");
+        } catch (ElementNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+}
