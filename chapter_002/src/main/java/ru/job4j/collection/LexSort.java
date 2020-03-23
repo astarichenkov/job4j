b2411f51ea583555f105feec76f042ea833caa61
Anton Starichenkov <anton.starichenkov@gmail.com>
2020-03-24 00:11:48 +0300

    4. Сортировка номера.[#224324]

--- chapter_002/src/main/java/ru/job4j/collection/LexSort.java
+++ chapter_002/src/main/java/ru/job4j/collection/LexSort.java
@@ -6,22 +6,8 @@ public class LexSort implements Comparator<String> {
 
     @Override
     public int compare(String left, String right) {
-        String s1 = "";
-        for (int i = 0; i < left.length(); i++) {
-            if (left.charAt(i) >= '0' && left.charAt(i) <= '9') {
-                s1 += left.charAt(i);
-            } else if (left.charAt(i) == '.') {
-                break;
-            }
-        }
-        String s2 = "";
-        for (int i = 0; i < right.length(); i++) {
-            if (right.charAt(i) >= '0' && right.charAt(i) <= '9') {
-                s2 += right.charAt(i);
-            } else if (right.charAt(i) == '.') {
-                break;
-            }
-        }
+        String s1 = left.substring(0, left.indexOf("."));
+        String s2 = right.substring(0, right.indexOf("."));
         return Integer.parseInt(s1) - Integer.parseInt(s2);
     }
 
