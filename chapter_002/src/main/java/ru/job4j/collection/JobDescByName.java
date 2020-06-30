50eccd4dbb8d91e4dd832e0e80bd7ad0c2d1f78e
Anton Starichenkov <anton.starichenkov@gmail.com>
2020-06-30 23:28:38 +0300

    3. Ссылки на методы.[70608#224199]

--- chapter_002/src/main/java/ru/job4j/collection/JobDescByName.java
+++ chapter_002/src/main/java/ru/job4j/collection/JobDescByName.java
@@ -5,6 +5,7 @@ import java.util.Comparator;
 public class JobDescByName implements Comparator<Job> {
     @Override
     public int compare(Job o1, Job o2) {
+        System.out.println(o2.getName().compareTo(o1.getName()));
         return o2.getName().compareTo(o1.getName());
     }
 }
