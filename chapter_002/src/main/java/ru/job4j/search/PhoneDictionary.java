c50ccda589af5562d68bb2b46667bf7311d00fd7
Anton Starichenkov <anton.starichenkov@gmail.com>
2020-07-03 09:57:25 +0300

    6. В классе StartUI заменить вывод в консоль на Consumer.[24255#224197]

--- chapter_002/src/main/java/ru/job4j/search/PhoneDictionary.java
+++ chapter_002/src/main/java/ru/job4j/search/PhoneDictionary.java
@@ -1,6 +1,8 @@
 package ru.job4j.search;
 
 import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.function.Predicate;
 
 public class PhoneDictionary {
     private ArrayList<Person> persons = new ArrayList<Person>();
@@ -11,13 +13,20 @@ public class PhoneDictionary {
 
     /**
      * Вернуть список всех пользователей, который содержат key в любых полях.
+     *
      * @param key Ключ поиска.
      * @return Список подощедщих пользователей.
      */
+
     public ArrayList<Person> find(String key) {
+        Predicate<Person> personName = (person) -> person.getName().equals(key);
+        Predicate<Person> personSurname = (person) -> person.getSurname().equals(key);
+        Predicate<Person> personAddress = (person) -> person.getAddress().equals(key);
+        Predicate<Person> personPhone = (person) -> person.getPhone().equals(key);
+        Predicate<Person> combine = personName.or(personSurname).or(personAddress).or(personPhone);
         ArrayList<Person> result = new ArrayList<>();
         for (Person person : persons) {
-            if (person.getAddress().contains(key) || person.getName().contains(key) || person.getPhone().contains(key) || person.getSurname().contains(key)) {
+            if (combine.test(person)) {
                 result.add(person);
             }
         }
