dd45a55c1d158608a1782579fa3d71c2ead28a88
Anton Starichenkov <anton.starichenkov@gmail.com>
2020-07-05 00:43:10 +0300

    3. Уникальность элементов и сортировка.[110225#224207]

--- chapter_004/src/main/java/ru/job4j/stream/Profiles.java
+++ chapter_004/src/main/java/ru/job4j/stream/Profiles.java
@@ -1,11 +1,22 @@
 package ru.job4j.stream;
 
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class Profiles {
+    private Comparator sorted = new Comparator() {
+        @Override
+        public int compare(Object o1, Object o2) {
+            Profile left = (Profile) o1;
+            Profile right = (Profile) o2;
+            return left.getAddress().getCity().compareTo(right.getAddress().getCity());
+        }
+    };
+
     List<Address> collect(List<Profile> profiles) {
+        profiles.sort(sorted);
         return profiles.stream().map(
-                profile -> profile.getAddress()).collect(Collectors.toList());
+                profile -> profile.getAddress()).distinct().collect(Collectors.toList());
     }
 }
--- chapter_004/src/test/java/ru/job4j/stream/ProfilesTest.java
+++ chapter_004/src/test/java/ru/job4j/stream/ProfilesTest.java
@@ -11,15 +11,32 @@ import static org.junit.Assert.assertThat;
 
 public class ProfilesTest {
     @Test
-    public void whenTwoAddresses() {
+    public void whenTwoAddressesAndSorted() {
         ArrayList<Profile> profilesList = new ArrayList<Profile>(Arrays.asList(
                 new Profile(new Address("Нижний Новгород", "Веденяпина", 4, 55)),
                 new Profile(new Address("Дзержинск", "Чапаева", 22, 13))));
         Profiles profiles = new Profiles();
         List<Address> result = profiles.collect(profilesList);
         ArrayList<Address> expected = new ArrayList<Address>(Arrays.asList(
-                (new Address("Нижний Новгород", "Веденяпина", 4, 55)),
-                (new Address("Дзержинск", "Чапаева", 22, 13))));
+                (new Address("Дзержинск", "Чапаева", 22, 13)),
+                (new Address("Нижний Новгород", "Веденяпина", 4, 55))));
+        assertThat(result, is(expected));
+    }
+
+    @Test
+    public void whenFourAddressesAndDoublesAndSorted() {
+        ArrayList<Profile> profilesList = new ArrayList<Profile>(Arrays.asList(
+                new Profile(new Address("Нижний Новгород", "Веденяпина", 4, 55)),
+                new Profile(new Address("Дзержинск", "Чапаева", 22, 13)),
+                new Profile(new Address("Казань", "Кул Гали", 17, 98)),
+                new Profile(new Address("Нижний Новгород", "Веденяпина", 4, 55))));
+        Profiles profiles = new Profiles();
+        ArrayList<Address> expected = new ArrayList<Address>(Arrays.asList(
+                (new Address("Дзержинск", "Чапаева", 22, 13)),
+                (new Address("Казань", "Кул Гали", 17, 98)),
+                (new Address("Нижний Новгород", "Веденяпина", 4, 55))));
+
+        List<Address> result = profiles.collect(profilesList);
         assertThat(result, is(expected));
     }
 }
