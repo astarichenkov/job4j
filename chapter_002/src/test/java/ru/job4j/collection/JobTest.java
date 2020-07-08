8f95fe4e8d57a3e98e871d88629185b7dd100c3d
Anton Starichenkov <anton.starichenkov@gmail.com>
2020-07-08 22:27:05 +0300

    1. Collection API Улучшения[70623#224212]

--- chapter_002/src/test/java/ru/job4j/collection/JobTest.java
+++ chapter_002/src/test/java/ru/job4j/collection/JobTest.java
@@ -1,6 +1,7 @@
 package ru.job4j.collection;
 
 import org.junit.Test;
+
 import java.util.Comparator;
 
 import static org.hamcrest.Matchers.greaterThan;
@@ -10,7 +11,8 @@ import static org.junit.Assert.assertThat;
 public class JobTest {
     @Test
     public void whenCompatorByNameAndPriority() {
-        Comparator<Job> cmpNamePriority = new JobDescByName().thenComparing(new JobDescByPriority());
+        Comparator<Job> cmpNamePriority = new JobDescByName()
+                .thenComparing(new JobDescByPriority());
         int rsl = cmpNamePriority.compare(
                 new Job("2", 1),
                 new Job("1", 2)
@@ -65,13 +67,12 @@ public class JobTest {
 
     @Test
     public void whenCompatorByPriorityAndName() {
-        Comparator<Job> cmpNamePriority = new JobDescByPriority().thenComparing(new JobDescByName());
+        Comparator<Job> cmpNamePriority = new JobDescByPriority()
+                .thenComparing(new JobDescByName());
         int rsl = cmpNamePriority.compare(
                 new Job("Name1", 1),
                 new Job("Name0", 1)
         );
         assertThat(rsl, lessThan(0));
     }
-
-
 }
